59a754a950e358bff17a7e310229f1ddb548ca62
samyprez <samyprez@gmail.com>
2026-08-06 17:28:41 -0400

    Add protected dashboard route

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import SamyOSApp from "@/app/components/SamyOSApp";
+import VoiceCommand from "@/app/components/VoiceCommand";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <SamyOSApp />
+      <VoiceCommand />
+    </>
+  );
+}
